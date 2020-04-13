bc51b12b9c036c2f26f5cd4998b20c74e9b0ab73
Allen Goodman <allen.goodman@icloud.com>
2020-04-12 20:48:52 -0400

    human.99.gff3

--- skgenome/data/gff3/__init__.py
+++ skgenome/data/gff3/__init__.py
@@ -1,9 +1,13 @@
 import os.path
 
+import skgenome.data
+
 
 def human(release="99"):
-    resource = os.path.join(
-        "ftp://ftp.ensembl.org",
-        f"pub/release-{release}/gff3/homo_sapiens/",
-        f"/Homo_sapiens.GRCh38.{release}.gff3.gz",
-    )
+    filename = f"human.{release}.gff3"
+
+    origin = f"ftp://ftp.ensembl.org/pub/release-{release}/gff3/homo_sapiens/Homo_sapiens.GRCh38.{release}.gff3.gz"
+
+    pathname = skgenome.data.get(filename, origin)
+
+    return pathname
