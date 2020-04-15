53c15be27da26f4e7a528d669ec76f2ed478f641
Allen Goodman <allen.goodman@icloud.com>
2020-04-14 20:10:20 -0400

    lark

--- setup.py
+++ setup.py
@@ -32,7 +32,12 @@ setuptools.setup(
         ],
         "test": ["coverage>=5.0.4", "pytest>=5.4.1"],
     },
-    install_requires=["matplotlib>=3.2.1", "numpy>=1.18.2", "pandas>=1.0.3"],
+    install_requires=[
+        "lark-parser>=0.8.5",
+        "matplotlib>=3.2.1",
+        "numpy>=1.18.2",
+        "pandas>=1.0.3",
+    ],
     long_description=long_description,
     long_description_content_type="text/markdown",
     name="scikit-genome",
